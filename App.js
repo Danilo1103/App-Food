515cb9347dff0e85c67830e082194e298ab0b43f
Danilo Ibañez <94373834+Danilo1103@users.noreply.github.com>
2022-10-08 15:11:48 -0500

    Bottom-Tab

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
+import { NavigationContainer } from '@react-navigation/native'
 import React from 'react'
-import { Text, View } from 'react-native'
-import Menu from './src/components/Menu.jsx'
-import Home from './src/components/Home.jsx'
-import AppBar from './src/components/AppBar.jsx'
-import { NativeRouter, Route, Routes } from 'react-router-native'
-
-const App = () => (
-  <NativeRouter>
-      <View>
-        <AppBar/>
-        <Routes>
-        <Route exact path='/' element={<Home />}/>
-        <Route exact path='/menu' element={<Menu />}/>
-        </Routes>
-        </View>
-  </NativeRouter>
+import { BottomTab } from './src/navigation/BottomTab'
+import { StatusBar } from 'expo-status-bar'
+const App = () => {
+  return (
+    <>
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <BottomTab />
+      </NavigationContainer>
+    </>
   )
+}
 
 export default App
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,21 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import Constants from 'expo-constants'
+import Carousel from 'react-bootstrap/Carousel';
+import Bug from '../../img/Bug.jpg' 
+import Bug02 from '../../img/Bug02.jpg' 
+import Piz from '../../img/Piz.jpg' 
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Constants.statusBarHeight + 10,
+  }
+})
 
 const Home = () => {
     return (
-        <View>
-            <Text>Trabajando...</Text>
-            <Text>Holaaaa</Text>
+        <View style={styles.container}>
+            <Text>Trabajando en el home...</Text>
         </View>
     )
 }
